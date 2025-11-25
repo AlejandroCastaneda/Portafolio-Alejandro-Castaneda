d9ddd9c8fad2d1b74ecaed89ce8b4e8bfe804117
Alejandro Castañeda <daco1994vr@gmail.com>
2025-11-25 15:59:57 -0500

    Cambio en perfil

--- components/molecules/about/index.tsx
+++ components/molecules/about/index.tsx
@@ -5,14 +5,9 @@ export default function About() {
             <Text text="Contacto"/>
             <div className="flex gap-5 justify-between px-px">
                 <div className=" flex-col self-start">
-                    <div className="mt-5"><PrincipalText text="Teléfono"/></div>
-                    <div className="mt-5"><PrincipalText text="Correo"/></div>
-                    <div className="mt-5"><PrincipalText text="Ubicación"/></div>
-                </div>
-                <div className="flex flex-col text-right">
-                    <div className="mt-6"><PrincipalText text="308340391"/></div>
-                    <div className="mt-6"><PrincipalText text="daco1994vr@gm"/></div>
-                    <div className="mt-6"><PrincipalText text="Medellín"/></div>
+                    <div className="mt-5"><PrincipalText text="308340391"/></div>
+                    <div className="mt-5"><PrincipalText text="daco1994vr@gmail.com"/></div>
+                    <div className="mt-5"><PrincipalText text="Medellín"/></div>
                 </div>
             </div>
         </div>
